344248c5ffc01ea74fb903c659c9cbde435e2a26
Carly Tarett <carlytarett@hotmail.com>
2020-02-12 17:23:50 +0000

    Tried hard coding the insert again

--- src/App.js
+++ src/App.js
@@ -12,27 +12,27 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    tasks: [ ]
+    tasks: []
   };
 
-// special built in method runs whenever mounts
-  componentDidMount(){
+  // special built in method runs whenever mounts
+  componentDidMount() {
     //fetch the tasks making a GET request
     axios.get(" https://ma5s5cp7sc.execute-api.eu-west-1.amazonaws.com/dev/tasks")
-        .then( (response) => {
-          const tasks = response.data.tasks;
-          this.setState({
-            tasks: tasks
-          })
-          console.log(response);
+      .then((response) => {
+        const tasks = response.data.tasks;
+        this.setState({
+          tasks: tasks
         })
-        .catch((err) => {
+        console.log(response);
+      })
+      .catch((err) => {
         console.log(err);
-        })
+      })
     // then set them as the state 
   }
 
-     inputNewTask = (description, dueDate) => {
+  inputNewTask = (description, dueDate) => {
     // put the first ones to match the database as it's what's sent
     // after the : it's parameters from the functions in this code
     const newTask = {
@@ -43,19 +43,19 @@ class App extends React.Component {
     };
 
     axios.post(" https://ma5s5cp7sc.execute-api.eu-west-1.amazonaws.com/dev/tasks", newTask)
-    .then( (response) => {
-    
+      .then((response) => {
+
         const newReturnedTask = response.data;
         const copyOfTasks = this.state.tasks.slice();
         copyOfTasks.push(newReturnedTask);
 
         this.setState({
           tasks: copyOfTasks
-        })   
-    })
-    .catch((err) => {
-    console.log(err);
-    });
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   // deleteTask = id => {
@@ -76,7 +76,7 @@ class App extends React.Component {
 
   deleteTask = id => {
     axios.delete(`https://ma5s5cp7sc.execute-api.eu-west-1.amazonaws.com/dev/tasks/${id}`)
-    .then( () => {
+      .then(() => {
         const filteredTasks = this.state.tasks.filter(task => {
           if (task.id === id) return false;
           else return true;
@@ -85,11 +85,11 @@ class App extends React.Component {
         this.setState({
           tasks: filteredTasks
         });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
- };
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
 
 
@@ -109,87 +109,105 @@ class App extends React.Component {
 
   markDone = id => {
 
-    const filteredTasks = this.state.tasks.map(task => {
+    axios.put(`https://ma5s5cp7sc.execute-api.eu-west-1.amazonaws.com/dev/tasks/${id}`, {
+      status: "completed"
+    })
+      .then(() => {
 
-      if (task.id !== id)
-        return task;
-      else
-        task.status = "completed"
-      return task;
-    });
-    console.log(filteredTasks);
-    this.setState({
-      tasks: filteredTasks
-    });
-  };
-
-  render() {
-    const liveTasks = this.state.tasks.filter(task => {
-      return task.status === "live";
-    });
-
-    const completedTasks = this.state.tasks.filter(task => {
-      return task.status === "completed";
-    });
+        console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxx");
+        console.log("id is :   ");
+        console.log(id);
+        console.log("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYy");
 
 
-    return (
-      <div className="App">
+        const filteredTasks = this.state.tasks.map(task => {
 
-        <div className="container">
+          if (task.id === id)
+            task.status = "completed";
 
-          <div className="row">
-            <div className="col-2 col-sm-2 edge">
-              <div style={{ height: "100vh" }}> </div>
-            </div>
+          return task;
+        });
 
-            <div className="col-8 col-sm-8">
+        console.log(filteredTasks);
 
-              <Header />
+        this.setState({
+          tasks: filteredTasks
+        })
+      })
 
-              <InputTask className="inputtask" inputTaskFunc={this.inputNewTask} />
-              <TaskCount number={liveTasks.length} />
-
-              <p>Current Tasks</p>
-
-              {liveTasks.map(task => {
-                return (
-                  <Task
-                    deleteTaskFunc={this.deleteTask}
-                    markTaskDoneFunc={this.markDone}
-                    status={task.status}
-                    dueDate={task.due_date}
-                    id={task.id}
-                    text={task.description}
-                  />
-                );
-              })}
-
-              <scan id="completed" > </scan>
-              <p >Completed Tasks</p>
-              {completedTasks.map(task => {
-                return (
-                  <Task
-                    deleteTaskFunc={this.deleteTask}
-                    status={task.status}
-                    id={task.id}
-                    text={task.description}
-                  />
-                );
-              })}
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
 
+    render() {
+      const liveTasks = this.state.tasks.filter(task => {
+        return task.status === "live";
+      });
 
-            </div>
-            <div className="col-2 col-sm-2 edge">
-              <div> </div>
+      const completedTasks = this.state.tasks.filter(task => {
+        return task.status === "completed";
+      });
+
+
+      return (
+        <div className="App">
+
+          <div className="container">
+
+            <div className="row">
+              <div className="col-2 col-sm-2 edge">
+                <div style={{ height: "100vh" }}> </div>
+              </div>
+
+              <div className="col-8 col-sm-8">
+
+                <Header />
+
+                <InputTask className="inputtask" inputTaskFunc={this.inputNewTask} />
+                <TaskCount number={liveTasks.length} />
+
+                <p>Current Tasks</p>
+
+                {liveTasks.map(task => {
+                  return (
+                    <Task
+                      deleteTaskFunc={this.deleteTask}
+                      markTaskDoneFunc={this.markDone}
+                      status={task.status}
+                      dueDate={task.due_date}
+                      id={task.id}
+                      text={task.description}
+                    />
+                  );
+                })}
+
+                <scan id="completed" > </scan>
+                <p >Completed Tasks</p>
+                {completedTasks.map(task => {
+                  return (
+                    <Task
+                      deleteTaskFunc={this.deleteTask}
+                      status={task.status}
+                      id={task.id}
+                      text={task.description}
+                    />
+                  );
+                })}
+
+
+
+              </div>
+              <div className="col-2 col-sm-2 edge">
+                <div> </div>
+              </div>
             </div>
           </div>
+
         </div>
-
-      </div>
-    );
+      );
+    }
   }
-}
 
-export default App;
+  export default App;
