49bfa4846bf8f60ebcfa809bcee7da9f3befef75
Carly Tarett <carlytarett@hotmail.com>
2020-02-25 20:34:02 +0000

    Updates

---
+++ precache-manifest.33f5a18974d5e36fe1d5ea9120892b97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f380691d5e4bd472b3aee5c9f717ea5",
+    "url": "/ReactToDo/index.html"
+  },
+  {
+    "revision": "b6525649eb57752d66a8",
+    "url": "/ReactToDo/static/css/main.2f9d11ee.chunk.css"
+  },
+  {
+    "revision": "6de389aa10ea61b5b39c",
+    "url": "/ReactToDo/static/js/2.090cacfd.chunk.js"
+  },
+  {
+    "revision": "b6525649eb57752d66a8",
+    "url": "/ReactToDo/static/js/main.af862f9b.chunk.js"
+  },
+  {
+    "revision": "60d2964dc3a95d68d38c",
+    "url": "/ReactToDo/static/js/runtime-main.3785f464.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ReactToDo/static/media/logo.25bf045c.svg"
+  }
+]);
